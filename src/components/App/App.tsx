83c6d74848571620286c6949293773fefdea2e74
BastD29 <bastiendaniel9@gmail.com>
2024-06-27 17:19:26 +0200

    added useIdle custom hook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
-import InactivityDetector from "../InactivityDetector/InactivityDetector";
+// import InactivityDetector from "../InactivityDetector/InactivityDetector";
+import useIdle from "../../hooks/useIdle";
 import style from "./App.module.scss";
 
 const App: FC = () => {
+  const isIdle = useIdle(3000); // 3 seconds
   return (
     <div className={style["app"]}>
-      <InactivityDetector />
+      {/* <InactivityDetector /> */}
+      {isIdle ? <p>User is idle</p> : <p>User is active</p>}
     </div>
   );
 };
